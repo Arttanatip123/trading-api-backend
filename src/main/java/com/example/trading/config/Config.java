fecf72ed210958966c0af5130ff000bd8373852e
Tanatip Seemoolkam <43342075+Arttanatip123@users.noreply.github.com>
2021-02-26 23:56:35 +0700

    fix path img

--- src/main/java/com/example/trading/config/Config.java
+++ src/main/java/com/example/trading/config/Config.java
@@ -1,8 +1,12 @@
 package com.example.trading.config;
 
 public class Config {
-    public static final String IMG_SHOP_URL = "C:/img/shop/";
-    public static final String IMG_PRODUCT_URL = "C:/img/product/";
+//    public static final String IMG_SHOP_URL = "C:/img/shop/";
+//    public static final String IMG_PRODUCT_URL = "C:/img/product/";
+
+    public static final String IMG_SHOP_URL = "home/tanatip99950/shop/";
+    public static final String IMG_PRODUCT_URL = "home/tanatip99950/product/";
+
 
     public static final int DASHBOARD_MOREDATA_SIZE = 10;
 
